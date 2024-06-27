d38c0bc00e2af7adc443dd61e93c70bf60a6b21e
Ismail007 <shakeIsmail007@gmail.com>
2024-06-27 19:54:56 +0530

    Day-2 commit

---
+++ Day-2/Datatypes_bytes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Datatypes_bytes{
+    public static void main(String args[]){
+        byte b = 125;
+        String c = "\u0025";
+
+        char v = toChar(c);
+//        System.out.println("Enter a Unicode");
+//        Scanner input = new Scanner(System.in);
+//        String w = input.nextLine();
+//        char t = w.toChar();
+    }
+}
---
+++ Day-2/ScannerUtil.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ScannerUtil {
+    public static void main(String args[]){
+        Scanner sin = new Scanner(System.in);
+        System.out.println("Enter a name: " + sin.nextLine());
+    }
+}
